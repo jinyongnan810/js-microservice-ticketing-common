bfec7c8f5942f6871f67ec6d2ec4ffb60dbaf3a7
Yuunan Kin <yuunan.kin@gmail.com>
2021-01-14 08:21:57 +0900

    expiration event

--- src/index.ts
+++ src/index.ts
@@ -17,5 +17,6 @@ export * from "./events/events/ticket-created-event";
 export * from "./events/events/ticket-updated-event";
 export * from "./events/events/order-cancelled-event";
 export * from "./events/events/order-created-event";
+export * from "./events/events/expiration-complete-event";
 
 export * from "./events/types/order-status";
